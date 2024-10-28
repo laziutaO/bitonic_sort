4f71087a3bb6495ef73b7b6d10a0eeb7d0dfaaf0
Oleksii Laziuta <88631922+laziutaO@users.noreply.github.com>
2024-10-28 22:42:02 +0200

    updated parallel sort

--- parallel_sort.py
+++ parallel_sort.py
@@ -10,15 +10,19 @@ def bitonic_merge(arr, start, sub_len, asc, comparator):
     k = sub_len // 2
     while k > 0:
         for i in range(start, start + sub_len - k):
-            if i + k < len(arr):  
+            if i + k < len(arr):
                 swap_elements(arr, i, i + k, asc, comparator)
         k //= 2
 
-def batch_split_swap(shared_arr, start_indices, size, comparator):
+
+def batch_split_swap(shared_arr, start_indices, size, comparator, pool):
     half_size = size // 2
+    tasks = []
     for start in start_indices:
-        bitonic_merge(shared_arr, start, half_size, True, comparator)
-        bitonic_merge(shared_arr, start + half_size, half_size, False, comparator)
+        tasks.append(pool.apply_async(bitonic_merge, args=(shared_arr, start, half_size, True, comparator)))
+        tasks.append(pool.apply_async(bitonic_merge, args=(shared_arr, start + half_size, half_size, False, comparator)))
+    [task.wait() for task in tasks]
+
 
 def is_power_of_2_log(n):
     if n <= 0:
@@ -30,18 +34,21 @@ def parallel_bitonic_sort(arr, length, workers, asc, comparator):
     if not is_power_of_2_log(length):
         raise ValueError("Length of the array must be a power of 2")
     size = 2
-    max_worker = workers
+    shared_arr = multiprocessing.Array('i', arr)
 
-    pool = multiprocessing.Pool(processes=max_worker)
     while size <= length:
-        batch_size = length // max_worker
+        batch_size = length // workers
         batches = [range(i, min(i + batch_size, length), size) for i in range(0, length, batch_size)]
-        result = [pool.apply_async(batch_split_swap, args=(arr, list(batch), size, asc, comparator)) for batch in batches]
-
-        for start in range(0, length, size):
-            bitonic_merge(arr, start, size, asc, comparator)
+        with multiprocessing.Pool(workers) as pool:
+            tasks = [
+                pool.apply_async(batch_split_swap, (shared_arr, list(batch), size, comparator, pool))
+                for batch in batches
+            ]
+            [task.wait() for task in tasks]
 
         size *= 2
+
+    arr[:] = list(shared_arr)
 
 def is_sorted_parallel(arr, asc=True, comparator=None):
     if comparator is None:
@@ -55,3 +62,7 @@ def is_sorted_parallel(arr, asc=True, comparator=None):
             if comparator(arr[i], arr[i + 1]) < 0:
                 return False
     return True
+
+
+
+
